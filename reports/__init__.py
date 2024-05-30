8ef54875fb59c27cef013ca7b78a45f1a49e4d77
Musaab $aip <67084214+musaab123@users.noreply.github.com>
2024-05-30 20:57:15 +0400

    Delete reports directory

--- reports/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
-
-
-from . import account_partner_ledger
-from . import report_aged_partner
-from . import account_reconslition
-
-
