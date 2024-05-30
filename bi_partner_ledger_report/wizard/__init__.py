352fe3d4c304bfedbc039f6f96e4e5bfd9fe999b
Musaab $aip <67084214+musaab123@users.noreply.github.com>
2024-05-30 21:13:18 +0400

    Add files via upload

---
+++ bi_partner_ledger_report/wizard/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+from . import account_aged_partner_balance
+from . import excel_report
+from . import partner_ledger
+from . import partner_reconslition
+
+
